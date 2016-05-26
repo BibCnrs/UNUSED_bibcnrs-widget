f08b00cf8a0e4d12e31e4777d810a14087f33785
patrickkremer <patrick.kremer@inist.fr>
2016-05-26 15:08:02 +0200

    even test is successfull

--- test/e2e/test/header.js
+++ test/e2e/test/header.js
@@ -14,11 +14,22 @@ describe('Header', function() {
     it('should display hello world then sessionstorage.EBSCO_WIDGET_domain value once set', function (done) {
         browser
         .waitForElementVisible('.connexion', 1000)
-        .assert.containsText('.domain', 'Votre domaine principal : visite')
+        .assert.containsText('.domain', 'Connectez-vous !')
         .setSessionStorageKey('EBSCO_WIDGET_domain', 'insb')
         .pause(1000)
         .assert.containsText('.domain', 'Votre domaine principal : insb');
 
         client.start(done);
     });
+    it('should display hello world then sessionstorage.EBSCO_WIDGET_availableDomains value once set', function (done) {
+        browser
+        .waitForElementVisible('.connexion', 1000)
+        .assert.containsText('.otherDomains', '')
+        .setSessionStorageKey('EBSCO_WIDGET_availableDomains', '["insb","inshs"]')
+        .pause(1000)
+        .assert.containsText('.otherDomains', 'Les domaines possibles : insb, inshs');
+
+        client.start(done);
+    });
+
 });
